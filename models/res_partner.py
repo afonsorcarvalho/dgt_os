f3604f3413ba8d60e1a21570629f1c5bfdbb3193
afonsorcarvalho <afonso@diagnostica-ma.com.br>
2020-06-03 21:03:37 -0300

    Incluindo novos arquivos

---
+++ models/res_partner.py
@@ -0,0 +1,11 @@
+
+from odoo import fields, models, api
+
+class Partner(models.Model):
+    """Modelo que adiciona a funcionalidade de auto preencher o cadastro no formulário de res.partner, em caso de pessoa juridica"""
+    _inherit = 'res.partner'
+    maintenance_email = fields.Char('Email Manutenção')
+    allowed_payment_methods = fields.Many2many(
+        'account.payment.term',
+        string='Condições de Pagamento'
+    )
