50159cbc74fccc0fcce06052d8dee7ea16fa0f7c
Afonso Carvalho <afonso@jgma.com.br>
2021-11-30 02:02:59 -0300

    Concluindo calculo das diarias

--- models/dgt_os_plantao_viagem.py
+++ models/dgt_os_plantao_viagem.py
@@ -1,6 +1,7 @@
 ﻿# -*- coding: utf-8 -*-
 import time
 from datetime import datetime,timedelta
+from dateutil.relativedelta import *
 from odoo import models, api, fields, models, SUPERUSER_ID, _
 from odoo.addons import decimal_precision as dp
 from odoo import netsvc
@@ -151,18 +152,109 @@ class DgtOsPlantaoViagem(models.Model):
 		store=True 
 		)
 	
-	@api.depends('data_inicio','data_fim','tecnico_id','tipo')
+	@api.depends('data_inicio','data_fim','tecnico_id','tipo','relatorios_id')
 	def _compute_valor_total_diarias_a_pagar(self):
-		for record in self:
-			
+		for record in self:			
 			if record.data_fim:
 				_logger.info(record.data_fim - record.data_inicio)
 				days_date = record.data_fim - record.data_inicio
 				days = days_date.days
 				_logger.info(self.tecnico_id.name)
 				res_partner_tecnico = self.env['res.partner'].search([('name', '=', self.tecnico_id.name)], offset=0, limit=None, order=None, count=False)
-				record.valor_total_diarias_a_pagar = days*res_partner_tecnico.valor_diaria
+				record.valor_total_diarias_a_pagar = self._calcula_diarias(
+					record.tipo, res_partner_tecnico.valor_diaria, record.data_inicio , record.data_fim)
 	
+	def _calcula_diarias(self, tipo, valor_diaria, data_inicio, data_fim):
+		"""
+			Os cálculos do valor total de diárias são feitos da seguinte maneira: 
+			Caso tenha sido plantão sem atendimento será pago 50% da diária, se houver atendimento 100% da diária.
+			Caso seja um dia de sábado será pago 25% da diária sem atendimento e 50% se houver atendimenteo.
+			Para saber se houve atendimento, deverá ser colocado o relatório correspondente em relatórios vinculados.
+			Em caso de viagem 100% da diária
+
+		"""
+		if tipo == 'viagem':
+			_logger.debug("É VIAGEM")
+			#TODO 
+			# verificar se tem relatório de viagem
+			days_date = data_fim - data_inicio
+			days = days_date.days
+			return days*valor_diaria
+		if tipo == 'plantao':
+			_logger.debug("É PLANTAO")
+			_logger.debug("TEM ATENDIMENTO?")
+			days_diarias = self.verifica_data_week_relatorio(data_inicio,data_fim)
+			_logger.debug(days_diarias)
+			soma_diarias = 0
+			for diaria in days_diarias:
+				_logger.debug(diaria)
+				soma_diarias += diaria['porcentagem_diaria'] * valor_diaria
+			return soma_diarias	
+			
+	def verifica_data_week_relatorio(self,data_inicio,data_fim):
+		days_date = data_fim - data_inicio
+		days = days_date.days
+		days_diarias = []
+		relatorios_ids = self.relatorios_id
+		for seq in range(days+1):
+			_logger.debug("DATA")
+			date_current = data_inicio + relativedelta(days=+seq)
+			_logger.debug(date_current)
+			porcentagem_diaria = 0
+			for relatorio in relatorios_ids:
+				if relatorio.data_atendimento == date_current:
+					_logger.debug("TEM RELATORIO NESTA DATA")
+					if date_current.weekday() == 5: 
+						_logger.debug("É sabado")
+						
+						if relatorio.hora_termino > 12:
+							_logger.debug("Horario de termino do serviço é depois do 12:00hs")
+							porcentagem_diaria = 0.5
+							break
+						else:
+							_logger.debug("Horario de termino do serviço é antes das 12:00hs")
+							porcentagem_diaria = 0.25
+					else:
+						_logger.debug("Não é sabado")
+						porcentagem_diaria = 1
+						break
+				else:
+					_logger.debug("NÃO TEM RELATORIO NESTA DATA")
+					if date_current.weekday() == 5: 
+						_logger.debug("MAS É SABADO")
+						porcentagem_diaria = 0.25	
+					else:
+						porcentagem_diaria = 0.5	
+								
+			days_diarias.append({
+						'date': date_current,
+						'porcentagem_diaria': porcentagem_diaria
+			})
+		return days_diarias
+					
+
+
+
+	def tem_sabado(self,data_inicio, data_fim):
+		_logger.debug("TEM SABADO?")
+		days_date = data_fim - data_inicio
+		days = days_date.days
+		for seq in range(days):
+			_logger.debug("DATA")
+			date_current = data_inicio + relativedelta(days=+seq)
+			_logger.debug(date_current)
+			week = date_current.weekday()
+			_logger.debug(week)
+			if week == 5: 
+				return date_current
+			else:
+				_logger.debug("NÃO É SABADO")
+
+		return False
+
+
+
+
 	def verify_relatorios(self):
 		for record in self:
 			if len(record.relatorios_id) > 0:
@@ -172,7 +264,22 @@ class DgtOsPlantaoViagem(models.Model):
 	def _compute_sum_despesas_lines(self,col_name_sum):
 			sum_total = sum(self.env["dgt_os.os.plantao.viagem.despesas.line"].search([('rdv_id','=', self.id)]).mapped(col_name_sum))
 			return sum_total
-	
+
+
+	def set_state_em_aprovacao(self):
+		for record in self:
+			for despesa in record.despesas_line_id:
+				despesa.write({
+					'state': 'em_aprovacao'
+				})
+			record.write({
+					'state': 'em_aprovacao'
+				})
+
+#****************************************
+# 	ACTIONS
+# 
+#****************************************	
 	def action_confirmar(self):
 		
 		for record in self:
@@ -185,16 +292,6 @@ class DgtOsPlantaoViagem(models.Model):
 					raise UserError(
                 	_('É necessário que tenha relatórios vinculados para o caso de viagens!  Vincule o relatório de serviço de acordo com a data de viagem'))
 
-	def set_state_em_aprovacao(self):
-		for record in self:
-			for despesa in record.despesas_line_id:
-				despesa.write({
-					'state': 'em_aprovacao'
-				})
-			record.write({
-					'state': 'em_aprovacao'
-				})
-	
 	def action_provisorio(self):
 		for record in self:
 			record.write({
